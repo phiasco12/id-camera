01a1becec3c2bfc444b04113e772b44bbc03fa45
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-05-11 07:41:40 +0100

    Update cordova-camera-with-frame.js

--- www/cordova-camera-with-frame.js
+++ www/cordova-camera-with-frame.js
@@ -1,9 +1,8 @@
-var exec = require('cordova/exec');
-
-var CordovaCameraWithFrame = function() {};
-
-CordovaCameraWithFrame.prototype.openCamera = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, 'CordovaCameraWithFrame', 'openCamera', []);
+var CordovaCameraWithFrame = {
+    openCamera: function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'CordovaCameraWithFrame', 'openCamera', []);
+    }
 };
 
-module.exports = new CordovaCameraWithFrame();
+module.exports = CordovaCameraWithFrame;
+
