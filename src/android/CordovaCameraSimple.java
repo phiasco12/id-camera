865bb1a4ffdb2929c6732c0f8052c0475d1c32a6
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-05-11 08:31:09 +0100

    Update CordovaCameraSimple.java

--- src/android/CordovaCameraSimple.java
+++ src/android/CordovaCameraSimple.java
@@ -2,6 +2,9 @@ package com.example.cordova.camera;
 
 import android.content.Intent;
 import android.provider.MediaStore;
+import android.view.TextureView;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
@@ -28,6 +31,21 @@ public class CordovaCameraSimple extends CordovaPlugin {
             public void run() {
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (cameraIntent.resolveActivity(cordova.getActivity().getPackageManager()) != null) {
+                    // Create a TextureView to hold the camera preview
+                    TextureView textureView = new TextureView(cordova.getActivity());
+                    textureView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+                    // Add TextureView to the content view
+                    cordova.getActivity().setContentView(textureView);
+
+                    // Add ImageView programmatically
+                    ImageView imageView = new ImageView(cordova.getActivity());
+                    imageView.setImageResource(R.drawable.your_image); // Replace with your image resource
+                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                    imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                    cordova.getActivity().addContentView(imageView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+                    // Start camera intent
                     cordova.startActivityForResult(CordovaCameraSimple.this, cameraIntent, CAMERA_REQUEST);
                 } else {
                     callbackContext.error("Camera not available");
